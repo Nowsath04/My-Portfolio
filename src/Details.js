9c2347a9649d3c26b42ea3750a0ef0364c7d542d
Nowsath04 <110928124+Nowsath04@users.noreply.github.com>
2023-01-17 12:04:50 +0530

    Portfolio

---
+++ src/Details.js
@@ -0,0 +1,132 @@
+
+// Profile Image
+import profile from "./assets/1725265.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import nodejs from "./assets/techstack/Nodejs.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import mongodb from "./assets/techstack/mongo.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import mysql from "./assets/techstack/mysql.png";
+import ExpressJS from "./assets/techstack/ExpressJS.png";
+// Porject Images
+import projectImage1 from "./assets/projects/whatsapp-clone.JPG";
+import projectImage2 from "./assets/projects/student.JPG";
+import projectImage3 from "./assets/projects/pizza.JPG";
+
+
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Nowsath A",
+  tagline: "I build things for web",
+  img: profile,
+  about: `Hello! I'm Nowsath, a passionate Full Stack Developement. 
+  I develop web applications. My core skill is based on JavaScript and I love 
+  to do most of the things using JavaScript. I love to make the web more open 
+  to the world. I have graduated with a bachelor's degree in Computer Application 
+  from Nehru Arts And Science College at Coimbatore, TamilNadu in 2022. I am available
+   for any kind of job opportunity that suits my interests.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/nowsath-a-66a0b9255/",
+  github: "https://github.com/Nowsath04",
+  instagram: "https://www.instagram.com/nowsath___nichu____/",
+};
+
+export const eduDetails = [
+  {
+    Position: "Full Stack Developer (MERN)",
+    Company: "Guvi Geeks IIT Research Park",
+    Location: "Chennai",
+    Duration: "2022",
+  },
+  {
+    Position: "Bachelor Of Computer Application (BCA)",
+    Company: `Nehru Arts And Science College`,
+    Location: "Coimbatore",
+    Duration: "2019-2022",
+  },
+  {
+    Position: "Higher Secondary Certificate",
+    Company: `C.S.I Boys Hr Sec School`,
+    Location: "Coimbatore",
+    Duration: "2017-2019",
+  },
+  {
+    Position: "Secondary School Leaving Certificate",
+    Company: `C.S.I Boys Hr Sec School`,
+    Location: "Coimbatore",
+    Duration: "2016-2017",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  mysql: mysql,
+  mongodb: mongodb,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  ExpressJS:ExpressJS,
+  nodejs:nodejs
+
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Whatsapp-Clone",
+    image: projectImage1,
+    description: `Whatsapp-clone app allows you to communicate with your buddies through chat. It enables you to send and receive messages. Whatsapp-clone apps make 
+    it easier, simpler and faster to connect with everyone and it is also easy to use.`,
+    techstack: "MERN",
+    frontend: "https://github.com/Nowsath04/Whatsapp-clone-frondend",
+    backend: "https://github.com/Nowsath04/Whatsapp-clone-backend",
+    DeployUrl: "https://deft-smakager-85a14b.netlify.app",
+  },  {
+    title: "Zen Class Student Portal",
+    image: projectImage2,
+    description: `Zen Class Student Portal is an e-Learning application where registered
+     students can attend classes, avail courses and monitor their tasks & courses status.`,
+    techstack: "MERN",
+    frontend: "https://github.com/Nowsath04/Zen-frontend",
+    backend: "https://github.com/Nowsath04/zen-backend.git",
+    DeployUrl: "https://profound-capybara-7abab5.netlify.app",
+  },  {
+    title: "Pizza Delivery App",
+    image: projectImage3,
+    description: `This is a Full Stack (MERN) Pizza Delivery Application with admin panel developed using React and React for Front End , Redux-Thunk for Asynchronous operations,
+     Node JS for Runtime environment , Express JS for Backend Routing and Mongo DB for Database`,
+    techstack: "MERN",
+    frontend: "https://github.com/Nowsath04/Pizza-Frontend",
+    backend: "https://github.com/Nowsath04/Pizza-Backend",
+    DeployUrl: "https://shimmering-blancmange-07668d.netlify.app",
+  },
+  
+  
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "nowsathnichu@gmail.com",
+  phone: "+91 9080007277",
+};
---
+++ src/Pages/About.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Work from "../Components/Work";
+import { personalDetails, eduDetails } from "../Details";
+
+function About() {
+  return (
+    <main className="container mx-auto max-width pt-10 pb-20 ">
+      <section>
+        <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          About Me
+        </h1>
+        <p className="text-content py-8 lg:max-w-3xl">{personalDetails.about}</p>
+        <a className="Nowsath"
+        href="https://www.linkedin.com/in/nowsath-a-66a0b9255/" target="_blank"
+        >Resume</a>
+      </section>
+      <section>
+        <h1 className="text-2xl pt-10 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          Education
+        </h1>
+        {React.Children.toArray(
+          eduDetails.map(({ Position, Company, Location, Duration }) => (
+            <Work
+              position={Position}
+              company={Company}
+              location={Location}
+              duration={Duration}
+            />
+          ))
+        )}
+      </section>
+    </main>
+  );
+}
+
+export default About;
